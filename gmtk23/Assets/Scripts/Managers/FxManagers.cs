6914ea11c6c7fffa8acd07e0b92b89724c77712c
Mathis Jibert <mathis.jibert@gmail.com>
2023-07-09 01:27:56 +0200

    Je commit

--- gmtk23/Assets/Scripts/Managers/FxManagers.cs
+++ gmtk23/Assets/Scripts/Managers/FxManagers.cs
@@ -9,12 +9,18 @@ namespace Managers
     public class FxManagers : MonoSingleton<FxManagers>
     {
         [SerializeField] private ParticleSystem m_damageFx;
+        [SerializeField] private ParticleSystem m_humanBloodFx;
+        [SerializeField] private ParticleSystem m_orcBloodFx;
 
         private Pool<ParticleSystem> m_damagesPool;
+        private Pool<ParticleSystem> m_orcBloodPool;
+        private Pool<ParticleSystem> m_humanBloodPool;
 
         private void Start()
         {
             m_damagesPool = new Pool<ParticleSystem>(m_damageFx);
+            m_orcBloodPool = new Pool<ParticleSystem>(m_orcBloodFx);
+            m_humanBloodPool = new Pool<ParticleSystem>(m_humanBloodFx);
         }
 
         public static void RequestDamageFxAtPos(Vector3 position)
@@ -35,5 +41,25 @@ namespace Managers
             
             instance.StartCoroutine(instance.m_damagesPool.AddToPoolLatter(effect, 0.5f));
         }
+        
+        public static void RequestBloodFxAtPos(Vector3 position,bool orcBlood)
+        {
+            if (instance == null)
+            {
+                Logs.Log("Fx manager instance null ...", LogType.Error);
+                return;
+            }
+
+            var effect = orcBlood ? instance.m_orcBloodPool.GetFromPool() : instance.m_humanBloodPool.GetFromPool();
+            effect.gameObject.SetActive(true);
+            effect.Stop();
+
+            effect.transform.position = position;
+            
+            effect.Play();
+            
+            instance.StartCoroutine( orcBlood ? instance.m_orcBloodPool.AddToPoolLatter(effect, 0.5f) : instance.m_humanBloodPool.AddToPoolLatter(effect, 0.5f));
+        }
+        
     }
 }
